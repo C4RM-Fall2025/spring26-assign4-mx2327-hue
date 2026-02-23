713232797b4627acc773cd6c12c5a781b2cd89fa
mx2327-hue <mx2327@columbia.edu>
2026-02-22 21:25:41 -0500

    BondDuration.py

---
+++ BondDuration.py
@@ -0,0 +1,16 @@
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    periods = m * ppy
+    r = y / ppy
+    C = couponRate * face / ppy
+    price = 0
+    weighted_sum = 0
+    for t in range(1, periods + 1):
+        if t == periods:
+            cashflow = C + face
+        else:
+            cashflow = C
+        pv = cashflow / (1 + r) ** t
+        price += pv
+        weighted_sum += t * pv
+    duration = weighted_sum / price
+    return duration/y
