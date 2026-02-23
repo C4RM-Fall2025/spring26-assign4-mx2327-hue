f2006d8de6edc92f26467a9e89f57acf0366e4f7
mx2327-hue <mx2327@columbia.edu>
2026-02-22 20:17:10 -0500

    Add files via upload

---
+++ WhoAmI_File.py
@@ -0,0 +1,45 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "python",
+      "display_name": "Python (Pyodide)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "python",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.8"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "a7ee84ab-72c5-4ea6-857e-3e76e9f4306e",
+      "cell_type": "code",
+      "source": "def WhoAmI():\n    return \"mx2327\"",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": 3
+    },
+    {
+      "id": "8d8546c6-7b0f-4db9-b735-a4f93bc5823b",
+      "cell_type": "code",
+      "source": "",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    }
+  ]
+}
