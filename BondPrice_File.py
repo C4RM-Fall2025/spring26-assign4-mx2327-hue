bdf4c1802307fc9cf049dfa4e792e01b1104aaf9
mx2327-hue <mx2327@columbia.edu>
2026-02-22 22:57:32 -0500

    Delete BondPrice_File.py

--- BondPrice_File.py
+++
@@ -1,9 +0,0 @@
-def getBondPrice(y,face,couponRate,m,ppy=1):
-    periods = m * ppy
-    r = y / ppy
-    C = couponRate * face / ppy
-    price = 0
-    for t in range(1, periods + 1):
-        price += C / (1 + r) ** t
-    price += face / (1 + r) ** periods
-    return price
