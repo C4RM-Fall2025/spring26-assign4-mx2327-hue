5a83f1f516fb23118052f021771e6fb327f9d752
mx2327-hue <mx2327@columbia.edu>
2026-02-22 21:36:17 -0500

    BondPrice_Z.py

---
+++ BondPrice_Z.py
@@ -0,0 +1,11 @@
+def getBondPrice_Z(face, couponRate, times, yc):
+    C = couponRate * face
+    price = 0
+    for y, t in zip(yc, times):
+        if t == times[-1]:
+            cashflow = C + face
+        else:
+            cashflow = C
+        pv = cashflow / (1 + y) ** t
+        price += pv
+    return price
